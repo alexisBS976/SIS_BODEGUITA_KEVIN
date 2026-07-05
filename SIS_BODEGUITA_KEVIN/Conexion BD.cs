f5c67e836d53cbb8cf5777a5ba0c701071a7bc52
Alexis <abravosallca@ejemplo.com>
2026-07-05 16:55:29 -0500

    BASE LITE

--- SIS_BODEGUITA_KEVIN/Conexion BD.cs
+++ SIS_BODEGUITA_KEVIN/Conexion BD.cs
@@ -7,7 +7,6 @@ namespace SIS_BODEGUITA_KEVIN
 {
     public static class Conexion_BD
     {
-        // Código CORREGIDO (Removido 'Version=3;'):
         public static string Cadena = "Data Source=BD_BodeguitaKevin.db;";
     }
 }
